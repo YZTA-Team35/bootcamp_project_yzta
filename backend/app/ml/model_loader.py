4d7eda7bc6aba2b51e7057bd80ded9ebc9fd4568
Miray Piyade <miraypiyade@gmail.com>
2025-07-29 21:47:26 +0300

    model update3

--- backend/app/ml/model_loader.py
+++ backend/app/ml/model_loader.py
@@ -1,5 +1,10 @@
 from keras.models import load_model
 from pathlib import Path
+from keras.saving import enable_unsafe_deserialization
+
+
+enable_unsafe_deserialization();
+
 
 MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "best_model_epoch_01_val_acc_0.7063.keras"
 
