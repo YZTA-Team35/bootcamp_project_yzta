e8cb431802f07b35fc6fe94e3597cc789e93c16d
Miray Piyade <miraypiyade@gmail.com>
2025-07-29 22:17:28 +0300

    model update

--- backend/app/ml/model_loader.py
+++ backend/app/ml/model_loader.py
@@ -6,12 +6,11 @@ from keras.saving import enable_unsafe_deserialization
 enable_unsafe_deserialization();
 
 
-MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "best_model_epoch_01_val_acc_0.7063.keras"
-
+MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "best_efficientnet_finetuned.h5"
 model = None
 
 def get_model():
     global model
     if model is None:
-        model = load_model(MODEL_PATH, compile=False)
+        model = load_model(MODEL_PATH)
     return model
