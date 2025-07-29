c6178a88c37784df7423280310c7938bc1875b20
Miray Piyade <miraypiyade@gmail.com>
2025-07-29 20:31:19 +0300

    model update 2

--- backend/app/ml/model_loader.py
+++ backend/app/ml/model_loader.py
@@ -1,4 +1,20 @@
 from keras.models import load_model
+from keras.saving import enable_unsafe_deserialization  # 👈 Eklendi
+from pathlib import Path
+
+MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "best_model_epoch_01_val_acc_0.7063.keras"
+
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        enable_unsafe_deserialization()  # 👈 değiştirsen iyi olur
+        model = load_model(MODEL_PATH)
+    return model
+
+"""
+from keras.models import load_model
 from pathlib import Path
 
 # Yeni model yolu (yeni uzantıyla)
@@ -7,7 +23,8 @@ MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "best_model_epoch_0
 model = None
 
 def get_model():
-    global model
+    global modela
     if model is None:
         model = load_model(MODEL_PATH)
     return model
+"""
