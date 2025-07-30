f331f805af525a60431d515b10f07c8683754551
Miray Piyade <miraypiyade@gmail.com>
2025-07-30 18:29:27 +0300

    frontend canlı cors ayarı güncellendi

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,11 +18,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://localhost:5173",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
-        "https://bootcampprojectyzta-production.up.railway.app"
-    ],  # Sadece local ve Railway backend izinli
+        "https://bootcamp-project-yzta-r264.vercel.app"    ],  # Sadece local ve vercel frontend izinli
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
