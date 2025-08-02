0e72ffda2982d654b5031f6242b87a3ab8562fa4
Miray Piyade <miraypiyade@gmail.com>
2025-08-02 16:11:39 +0300

    model update

--- backend/app/ml/model_loader.py
+++ backend/app/ml/model_loader.py
@@ -1,11 +1,16 @@
 from keras.models import load_model
 from pathlib import Path
-MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "SkinDiseaseWeightsPre1.h5"  # .h5 dosyanın adı buysa
+
+MODEL_PATH = Path(__file__).resolve().parent.parent / "ml" / "SkinDiseaseWeightsPre1.h5"
 
 model = None
 
 def get_model():
     global model
     if model is None:
-        model = load_model(MODEL_PATH, compile=False)  # compile=False daha güvenli ve hızlı yükler
+        try:
+            model = load_model(MODEL_PATH, compile=False)  # lambda varsa dikkatli ol!
+        except Exception as e:
+            print(f"Model yüklenirken hata oluştu: {e}")
+            model = None
     return model
